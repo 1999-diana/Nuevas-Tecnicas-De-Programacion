50bb40a3e3fecac8376e1a9b74023a6a1c30ee87
1999-diana <60788389+1999-diana@users.noreply.github.com>
2021-06-24 12:06:24 -0500

    Trabajo en clase

--- Modulo_1/Programming/01_Projects/index.js
+++ Modulo_1/Programming/01_Projects/index.js
@@ -5,9 +5,32 @@ const app =express()
 
 //EndPoint
 app.get('/', (req,res) => {
-res.send("Hola mundo con ExpressJs")
-})
+res.send("Hola mundo con ExpressJs");
+});
+app.get("/saludo",(req,res)=>{
+   // req.query
+    //console.log(req.query);
+    //obtenemos la query del objeto request
+    const { 
+        query :{nombre, apellido},
+}=req;
+//const nombre=req.query.nombre
+//const apellido=req.query.apellido
+
+    res.json({
+        saludo:`Hola soy ${nombre},${apellido }` ,
+    });
+});
+
+app.get('/saludo/:nombre',(req,res)=>{
+    const {params}=req; 
+
+    //const nombre =req.params.nombre
+    res.json({
+        nombre: params.nombre,
+    });
+});
 
 app.listen(3000,()=>{
-    console.log("Servisor escuchando en http://localhost:3000 ");
-})
+    console.log("Servidor escuchando en http://localhost:3000 ");
+});
