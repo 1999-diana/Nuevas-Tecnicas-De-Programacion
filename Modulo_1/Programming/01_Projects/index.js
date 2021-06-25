e56edd5c9f5f665777c3c07520c63119852eaef2
1999-diana <60788389+1999-diana@users.noreply.github.com>
2021-06-25 13:36:14 -0500

    Trabjo de routes

--- Modulo_1/Programming/01_Projects/index.js
+++ Modulo_1/Programming/01_Projects/index.js
@@ -1,35 +1,27 @@
 const express =require('express');
 
 //genero una aplicacion express
-const app =express()
+const app =express();
 
-//EndPoint
-app.get('/', (req,res) => {
-res.send("Hola mundo con ExpressJs");
-});
-app.get("/saludo",(req,res)=>{
-   // req.query
-    //console.log(req.query);
-    //obtenemos la query del objeto request
-    const { 
-        query :{nombre, apellido},
-}=req;
-//const nombre=req.query.nombre
-//const apellido=req.query.apellido
+//Routes
+const { RouterIndex }=require('./routes/index');
 
-    res.json({
-        saludo:`Hola soy ${nombre},${apellido }` ,
-    });
-});
+app.use("/", RouterIndex)
+//app.use("/user", RouterIndex)
+//app.use("/user/data", RouterIndex)
+//app.use(RouterIndex)
 
-app.get('/saludo/:nombre',(req,res)=>{
-    const {params}=req; 
+/*
+"/"+ "/" ="//" =>"/"
+"/"+"/saludo"=>"//saludo"=>"/saludo"
+"/"+"/saludo/:nombre"="//saludo/:nombre"=>"/saludo/:nombre"
 
-    //const nombre =req.params.nombre
-    res.json({
-        nombre: params.nombre,
-    });
-});
+"/user"+"/"="/user"
+
+"/user"+"/saludo"="/user/saludo"
+"/user/data"+"/saludo"="/user/data/saludo"
+
+*/
 
 app.listen(3000,()=>{
     console.log("Servidor escuchando en http://localhost:3000 ");
--- Modulo_1/Programming/01_Projects/routes/index.js
+++ Modulo_1/Programming/01_Projects/routes/index.js
@@ -6,6 +6,28 @@ const router  =Router()
 
 //creacion de los endpoint
 
+router
+.get("/",(request,res) => {
+  res.send("Hola mundo con ExpressJs");
+})
+
+.get("/saludo",(req,res)=> {
+  const { query }=req;
+  res.json({
+    saludo:`Hola soy ${query.nombre} ${query.apellido }` ,
+  });
+})
+
+
+.get("/saludo/:nombre",(req,res)=> {
+  const {params}=req; 
+
+  //const nombre =req.params.nombre
+  res.json({
+      nombre: params.nombre,
+  });
+});
+
 //Exportamos las rutas
 module.exports.RouterIndex = router;
 
