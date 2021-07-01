e480e2770fa6745d04d050b92c462697bb75880a
1999-diana <60788389+1999-diana@users.noreply.github.com>
2021-06-30 19:35:38 -0500

    ConexionMongo

---
+++ Modulo_1/programming/01_Projects/index.js
@@ -0,0 +1,32 @@
+const express = require('express'); //requiero el paquete express
+
+//genero una aplicacion express 
+const app = express()
+//endpoint
+const{RouterIndex} = require('./routes/index')
+
+app.use("/", RouterIndex);
+//app.use("/user", RouterIndex);
+//app.use("/user/data", RouterIndex);
+//app.use(RouterIndex);cd 
+/*
+"/" + "/" = "//" => "/"
+"/" + "/saludo" = "//saludo" => "/saludo"
+"/" + "/saludo/:nombre" = "//saludo/:nombre" => "/saludo/:nombre"
+
+"/user" + "/saludo" = "/user/saludo"
+"/user/data" + "/saludo" = "/user/data/saludo" 
+*/
+
+app.listen(3000, () => {
+    console.log("Servidor escuchando en http://localhost:3000")
+})
+
+
+
+
+
+
+
+
+
